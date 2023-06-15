6e5b7b2ac2b10cf9bcce2dfeec4ef82722a17c58
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-06-15 15:12:33 -0400

    Initial starting point for the app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import GameBoard from './components/GameBoard';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <GameBoard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,10 @@
+import Card from './Card';
+
+export default function GameBoard() {
+
+    return (
+      <>
+        <div>hello world</div>
+      </>
+    )
+  }
